d5aa5d6468e7e10975f227d41b013af93bd4ca3c
Bruna Souza <71838731+Bruunaa@users.noreply.github.com>
2022-06-07 17:19:21 -0300

    RestControllers - Todos os clientes

--- src/main/java/com/api/helpr/resources/ClienteResource.java
+++ src/main/java/com/api/helpr/resources/ClienteResource.java
@@ -1,5 +1,8 @@
 package com.api.helpr.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.helpr.domain.Cliente;
+import com.api.helpr.domain.Tecnico;
 import com.api.helpr.domain.dtos.ClienteDTO;
+import com.api.helpr.domain.dtos.TecnicoDTO;
 import com.api.helpr.services.ClienteService;
 
 @RestController
@@ -24,4 +29,14 @@ public class ClienteResource {
 		Cliente obj = service.findById(id);
 		return ResponseEntity.ok().body(new ClienteDTO(obj));
 		}
+	
+	//Resposta todos os Clientes
+	@GetMapping
+	public ResponseEntity<List<ClienteDTO>> findAllCliente(){
+		List<Cliente> list = service.findAllClientes();
+		List<ClienteDTO> listDto = list.stream()
+				.map(tec -> new ClienteDTO(tec)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
 }
+
--- src/main/java/com/api/helpr/services/ClienteService.java
+++ src/main/java/com/api/helpr/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.api.helpr.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,10 @@ public class ClienteService {
 	public Cliente findById(Integer id) {
 		Optional<Cliente> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não foi encontrado: " + id));
+	}
+
+	public List<Cliente> findAllClientes() {
+		return repository.findAll();
 	}
 
 	
