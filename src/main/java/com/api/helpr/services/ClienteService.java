1958b7982a87a9f6882855e8a72869646f5ae753
Bruna Souza <71838731+Bruunaa@users.noreply.github.com>
2022-06-07 19:39:33 -0300

    Alteração de Dados do Cliente

--- src/main/java/com/api/helpr/services/ClienteService.java
+++ src/main/java/com/api/helpr/services/ClienteService.java
@@ -7,8 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.api.helpr.domain.Cliente;
+import com.api.helpr.domain.Pessoa;
+import com.api.helpr.domain.Cliente;
+import com.api.helpr.domain.dtos.ClienteDTO;
+import com.api.helpr.domain.dtos.ClienteDTO;
 import com.api.helpr.repositories.ClienteRepository;
 import com.api.helpr.repositories.PessoaRepository;
+import com.api.helpr.services.exceptions.DataIntegrityViolationException;
 import com.api.helpr.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -28,6 +33,36 @@ public class ClienteService {
 
 	public List<Cliente> findAllClientes() {
 		return repository.findAll();
+	}
+
+	//Método que fará a criação de novo cliente.
+	public Cliente create(ClienteDTO objDto) {
+		objDto.setId(null);
+		validaCpfEEmail(objDto);
+		Cliente newObj = new Cliente(objDto);
+		return repository.save(newObj);
+	}
+
+		//Método para modificar clientes existentes.
+		public Cliente update(Integer id, ClienteDTO objDto) {
+			objDto.setId(id);
+			Cliente oldObj = findById(id);
+			validaCpfEEmail(objDto);
+			oldObj = new Cliente(objDto);
+			return repository.save(oldObj);
+		}
+	
+	//Validará os CPFs e E-mails para update e create. 
+		private void validaCpfEEmail(ClienteDTO objDto) {
+
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDto.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDto.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+		obj = pessoaRepository.findByEmail(objDto.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDto.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+		}
 	}
 
 	
